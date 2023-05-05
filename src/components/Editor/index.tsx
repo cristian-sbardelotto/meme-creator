44322b9af5b4280fb650421375031c5bd5427291
Cristian Sbardelotto <cristian.k.sbardelotto@gmail.com>
2023-05-05 19:47:11 -0300

    feat(editor): finished the first structure of the editor

--- src/components/Editor/index.tsx
+++ src/components/Editor/index.tsx
@@ -1,16 +1,49 @@
+import { useState } from 'react';
+
+import Header from '../Header';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faKeyboard } from '@fortawesome/free-solid-svg-icons';
+
 import * as S from './styles';
 
 type EditorProps = {
   image: File;
-}
+};
 
 export default function Editor({ image }: EditorProps) {
+  const [text, setText] = useState<string>('');
+
   return (
-    <S.Container>
-      <S.Image
-        src={URL.createObjectURL(image)}
-        alt='Image selected by the user to create the meme'
-      />
-    </S.Container>
+    <>
+      <Header />
+
+      <S.Container>
+        <S.ImageGroup>
+          <div>
+            <p>{text}</p>
+          </div>
+
+          <img
+            src={URL.createObjectURL(image)}
+            alt='Image selected by the user to create the meme'
+          />
+        </S.ImageGroup>
+
+        <S.InputGroup className='das'>
+          <FontAwesomeIcon
+            icon={faKeyboard}
+            color='#777'
+          />
+
+          <S.Input
+            type='text'
+            placeholder='Text on top...'
+            value={text}
+            onChange={event => setText(event.target.value)}
+          />
+        </S.InputGroup>
+      </S.Container>
+    </>
   );
 }
--- src/components/Editor/styles.ts
+++ src/components/Editor/styles.ts
@@ -1,13 +1,60 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  height: 100vh;
+  padding-top: 10vh;
 
   display: flex;
-  flex-direction: column;
-  align-items: flex-start;
+  align-items: center;
+  justify-content: space-around;
 `;
 
-export const Image = styled.img`
-  height: 300px;
+export const ImageGroup = styled.div`
+  position: relative;
+
+  div {
+    width: 100%;
+    padding: 0 2rem;
+
+    position: absolute;
+  }
+
+  p {
+    text-align: center;
+    word-break: break-all;
+    font-size: 1.5rem;
+    font-family: Impact, sans-serif;
+  }
+
+  img {
+    height: 400px;
+    width: 500px;
+  }
+`;
+
+export const InputGroup = styled.div`
+  position: relative;
+
+  svg {
+    position: absolute;
+    top: 25%;
+    left: 90%;
+  }
+`;
+
+export const Input = styled.input`
+  padding: 0.4rem 5rem 0.4rem 1rem;
+
+  border: 3px solid #777;
+  border-radius: 7px;
+  font-weight: 700;
+  color: #fff;
+  background: #111;
+
+  transition: 0.1s all ease;
+
+  &:focus {
+    background: #1119;
+    outline: none;
+    border: 3px solid ${({ theme }) => theme.colors.primary};
+  }
 `;
